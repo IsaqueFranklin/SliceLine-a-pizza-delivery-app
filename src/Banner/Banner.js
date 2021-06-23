62fd90f4855d5619ac60a24e49886d63f029aa50
Isaque F <isaquefrankli@gmail.com>
2021-06-23 18:19:11 -0300

    finishing banner

--- src/Banner/Banner.js
+++ src/Banner/Banner.js
@@ -1,8 +1,10 @@
 import styled from 'styled-components'
 
 export const Banner = styled.div`
-    height: 200px;
+    height: 60vh;
     background-image: url("img/banner3.jpg");
     background-position: center;
+    background-repeat: no repeat;
     background-size: cover;
+    filter: contrast(90%);
 `
