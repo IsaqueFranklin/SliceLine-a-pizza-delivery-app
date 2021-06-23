b2ccb46113ccb081d16c798b00cff708ec48da98
Isaque F <isaquefrankli@gmail.com>
2021-06-23 19:07:15 -0300

    now its really finished

--- src/Data/FoodData.js
+++ src/Data/FoodData.js
@@ -14,5 +14,21 @@ export const foods = [
     {
         name: 'Rucula Pizza',
         img: '/img/rucula.jpg'
+    },
+    {
+        name: 'Pizza Slices',
+        img: '/img/slices.jpg'
+    },
+    {
+        name: 'Italian Pizza',
+        img: '/img/italian.jpg'
+    },
+    {
+        name: 'Roman Pizza',
+        img: '/img/roman.jpg'
+    },
+    {
+        name: 'Special Pizza',
+        img: '/img/special.jpg'
     }
 ]
