880daea61578bb6dac5e68e658516e2c0e139b4a
Isaque F <isaquefrankli@gmail.com>
2021-06-23 18:42:19 -0300

    working at the grid

---
+++ src/Data/FoodData.js
@@ -0,0 +1,18 @@
+export const foods = [
+    {
+        name: 'Cheese Pizza',
+        img: '/img/cheese.jpg'
+    },
+    {
+        name: 'Mini Pizza',
+        img: '/img/minipizza.jpg'
+    },
+    {
+        name: 'Family Pizza',
+        img: '/img/family.jpg'
+    },
+    {
+        name: 'Rucula Pizza',
+        img: '/img/rucula.jpg'
+    }
+]
