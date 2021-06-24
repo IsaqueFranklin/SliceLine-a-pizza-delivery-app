5462005553923d7d706ee993cbbe2c1ead084517
Isaque F <isaquefrankli@gmail.com>
2021-06-24 19:09:55 -0300

    orderlist

--- src/Order/Order.js
+++ src/Order/Order.js
@@ -20,6 +20,14 @@ const OrderContent = styled(DialogContent)`
     height: 100%;
 `;
 
+const OrderContainer = styled.div`
+    padding: 10px 0px;
+    border-bottom: 1px solid grey;
+`;
+
+const OrderItem = styled.div`
+    padding: 10px 0px;
+`;
 
 export function Order({orders}){
     return (
@@ -27,7 +35,18 @@ export function Order({orders}){
                 {orders.length === 0 
                 ? <OrderContent> 
                     Your order's looking pretty empty.
-                </OrderContent> : <OrderContent>Found {orders.length} orders</OrderContent>}
+                </OrderContent> 
+                : <OrderContent>
+                        {" "}
+                        <OrderContainer>Your Order:</OrderContainer>{" "}
+                        {orders.map(order => (
+                            <OrderContainer>
+                                <OrderItem>
+                                    {order.name}
+                                </OrderItem>
+                            </OrderContainer>
+                        ))}
+                </OrderContent>}
                 <DialogFooter>
                     <ConfirmButton>Checkout</ConfirmButton>
                 </DialogFooter>
