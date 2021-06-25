485827ed6e9f01079bc510607a9fd923958a6e69
Isaque F <isaquefrankli@gmail.com>
2021-06-25 17:50:54 -0300

    prices

--- src/Data/FoodData.js
+++ src/Data/FoodData.js
@@ -1,43 +1,58 @@
+export function formatPrice(price){
+    return price.toLocaleString('pt-BR', {
+        style: 'currency',
+        currency: 'brl'
+    })
+}
+
 export const FoodItems = [
     {
         name: 'Cheese Pizza',
         img: '/img/cheese.jpg',
-        section: 'pizza'
+        section: 'pizza',
+        price: 56
     },
     {
         name: 'Mini Pizza',
         img: '/img/minipizza.jpg',
-        section: 'pizza'
+        section: 'pizza',
+        price: 23
     },
     {
         name: 'Family Pizza',
         img: '/img/family.jpg',
-        section: 'pizza'
+        section: 'pizza',
+        price: 69
     },
     {
         name: 'Rucula Pizza',
         img: '/img/rucula.jpg',
-        section: 'pizza'
+        section: 'pizza',
+        price: 55
     },
     {
         name: 'Pizza Slices',
         img: '/img/slices.jpg',
-        section: 'pizza'
+        section: 'pizza',
+        price: 22
     },
     {
         name: 'Italian Pizza',
         img: '/img/italian.jpg',
-        section: 'pizza'
+        section: 'pizza',
+        price: 70
     },
     {
         name: 'Roman Pizza',
         img: '/img/roman.jpg',
-        section: 'pizza'
+        section: 'pizza',
+        price: 61
     },
     {
         name: 'Special Pizza',
         img: '/img/special.jpg',
-        section: 'pizza'
+        section: 'pizza',
+        price: 78
     }
 ];
 
